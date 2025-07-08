ec60322cbd2c8f3e7fb428443d9e76eddb4fce6f
mugishaamos00 <mugishaamos00@gmail.com>
2025-07-08 23:35:16 +0300

    Add files via upload

---
+++ no 1.py
@@ -0,0 +1,13 @@
+length= float(input("enter the length of the rectangle (in meters):"))
+width= float(input("enter the width of the rectangle (in meters):"))
+
+
+area = length * width
+perimeter = 2 * (length + width)
+
+
+print("\n===== Rectangle Measurements =====")
+print(f"Length: {length} m")
+print(f"Width: {width} m")
+print(f"Area: {area} square meters")
+print(f"Perimeter: {perimeter} meters")
