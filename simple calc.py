d0d1d777a4543de205705d2a33b2c34bd7dd7e58
mugishaamos00 <mugishaamos00@gmail.com>
2025-09-18 08:51:04 +0300

    Add files via upload

---
+++ simple calc.py
@@ -0,0 +1,33 @@
+# Simple Calculator
+
+while True:
+    print("\n--- Basic Calculator ---")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Exit")
+
+    choice = input("Choose an option (1-5): ")
+
+    if choice == "5":
+        print("Goodbye!")
+        break  # this stops the loop and exit program
+
+    # ask for numbers only if user didn’t choose Exit
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if choice == "1":
+        print("Result:", num1 + num2)
+    elif choice == "2":
+        print("Result:", num1 - num2)
+    elif choice == "3":
+        print("Result:", num1 * num2)
+    elif choice == "4":
+        if num2 != 0:
+            print("Result:", num1 / num2)
+        else:
+            print("Error: Cannot divide by zero!")
+    else:
+        print("Invalid choice, try again!")
