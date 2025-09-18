c82d9cbdeea8f364f9070b83bb3a82aabce4e14d
mugishaamos00 <mugishaamos00@gmail.com>
2025-09-18 14:02:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// VARIABLES
+// //containers that store data
+
+// let course_work_mark = 45; //number
+
+// let exam_mark = 30; //number
+
+// //const my_name = "Mugisha"; //string
+
+// let finalMark  = course_work_mark + exam_mark; //expression
+
+// console.log("The final score is:" + finalMark); //statement
+
+// console.log("When divided it Equates to :" + (course_work_mark + exam_mark )); //statement
+
+// console.log(course_work_mark* exam_mark); //statement
+
+
+// FUNCTIONS
+//blocks of code that perform a specific task
+
+const surname= "mugisha"; //string
+
+const othername= "Amos"; //string
+
+const full_name = surname + othername; 
+
+console.log (full_name); //statement
+
+console.log ("Welcome mr. " + surname + othername); //statement
